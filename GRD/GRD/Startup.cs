64fb055d040c53844855a95300ec69c1ce45a0f7
dinzone <dinamitdana1@gmail.com>
2018-09-12 00:14:50 +0300

    add db context only to ProjectContext

--- GRD/GRD/Startup.cs
+++ GRD/GRD/Startup.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using EFGetStarted.AspNetCore.NewDb.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.WebEncoders;
 using System.Text.Encodings.Web;
 using System.Text.Unicode;
+using GRD.Data;
 
 namespace GRD
 {
@@ -44,12 +39,10 @@ namespace GRD
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1).AddSessionStateTempDataProvider();
             services.AddSession();
 
-            var connection = @"Server=tcp:webproject2018.database.windows.net,1433;Initial Catalog=WebProject2018;Persist Security Info=False;
+            var connection = @"Server=tcp:webproject2018.database.windows.net,1433;Initial Catalog=DinTest;Persist Security Info=False;
                                User ID=ronavrham;Password=Dbpaa2018;MultipleActiveResultSets=False;Encrypt=True;
                                TrustServerCertificate=False;Connection Timeout=30;";
-            services.AddDbContext<ProductsContext>(options => options.UseSqlServer(connection));
-            services.AddDbContext<BranchesContext>(options => options.UseSqlServer(connection));
-            services.AddDbContext<UsersContext>(options => options.UseSqlServer(connection));
+            services.AddDbContext<ProjectContext>(options => options.UseSqlServer(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
