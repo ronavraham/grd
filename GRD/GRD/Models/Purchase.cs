6bf127db80394802b20b5bd572e41e0470a55b4a
dinzone <dinamitdana1@gmail.com>
2018-09-12 00:10:10 +0300

    add Purchase model

---
+++ GRD/GRD/Models/Purchase.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GRD.Models
+{
+    public class Purchase
+    {
+        [Key]
+        [Display(Name = "מזהה רכישה")]
+        public int Id { get; set; }
+        [Display(Name = "כמות")]
+        public int Count { get; set; }
+        [Display(Name = "תאריך רכישה")]
+        public DateTime PurchaseDate { get; set; }
+        [ForeignKey("ProductId")]
+        [Display(Name = "מוצר")]
+        public Product Product { get; set; }
+        [ForeignKey("BranchId")]
+        [Display(Name = "סניף")]
+        public Branch Branch { get; set; }
+        [ForeignKey("UserId")]
+        [Display(Name = "משתמש")]
+        public User User { get; set; }
+    }
+}
