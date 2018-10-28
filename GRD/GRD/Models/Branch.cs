c98431684f77405120ec84771e244631579982bd
dinzone <dinamitdana1@gmail.com>
2018-10-28 21:11:34 +0200

    add validation (client - server) to branch model

--- GRD/GRD/Models/Branch.cs
+++ GRD/GRD/Models/Branch.cs
@@ -10,16 +10,26 @@ namespace GRD.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required]
         [DisplayName("קו רוחב")]
         public double Lat { get; set; }
+        [Required]
         [DisplayName("קו אורך")]
         public double Long { get; set; }
+        [Required]
+        [StringLength(60, MinimumLength = 0)]
         [DisplayName("שם")]
         public string Name { get; set; }
+        [Required]
+        [StringLength(60, MinimumLength = 0)]
         [DisplayName("עיר")]
         public string City { get; set; }
+        [Required]
+        [StringLength(60, MinimumLength = 0)]
         [DisplayName("כתובת")]
         public string Address { get; set; }
+        [Required]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "המספר שהזנת אינו תקין")]
         [DisplayName("טלפון")]
         public string Telephone { get; set; }
         [DisplayName("פתוח בשבת")]
